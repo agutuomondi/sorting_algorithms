74a149e757d7cf293cf32189da326a8651f24392
agutuomondi <mwalimuagutu@gmail.com>
2024-01-21 09:34:22 -0800

    103-merge_sort.c

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -51,6 +51,7 @@ void merge_sort_recursive(int *subarr, int *buff, size_t front, size_t back)
 	if (back - front > 1)
 	{
 		size_t mid = front + (back - front) / 2;
+
 		merge_sort_recursive(subarr, buff, front, mid);
 		merge_sort_recursive(subarr, buff, mid, back);
 		merge_subarr(subarr, buff, front, mid, back);
