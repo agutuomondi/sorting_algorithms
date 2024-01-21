b29a219bd69fe9427b8564cee9c7a28a1a493e0e
agutuomondi <mwalimuagutu@gmail.com>
2024-01-21 02:27:13 -0800

    3-quick_sort.c

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -72,6 +72,7 @@ void lomuto_sort(int *array, size_t size, int left, int right)
 	if (right - left > 0)
 	{
 		int part = lomuto_partition(array, size, left, right);
+
 		lomuto_sort(array, size, left, part - 1);
 		lomuto_sort(array, size, part + 1, right);
 	}
